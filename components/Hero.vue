6ed19635c9bab073636dfd26924b132fb578fc1f
Connor Forsyth <connor@randomstate.co.uk>
2024-03-25 22:35:02 +0000

    Align svg and text colours with original design

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
       <div class="max-w-full mt-48 relative">
-        <WallOfText :rows="15" :columns="20" :me-position="11">
+        <WallOfText :rows="13" :columns="20" :me-position="11">
           <template v-slot:row_1>
             <div class="relative">
               <div class="leading-[64px] lg:leading-[100px] lg:mt-28 lg:ml-36 uppercase text-7xl lg:text-9xl font-brand font-bold text-cf-gray-500 z-10">Hire</div>
-              <HireMeArrow class="absolute z-10 left-40 -top-20 sm:-top-2 lg:top-4 lg:left-[400px] fill-current text-red-500 w-24 sm:w-[186px] h-[490px]"/>
+              <HireMeArrow class="absolute z-10 left-40 -top-20 sm:-top-2 lg:top-8 lg:left-[400px] w-24 sm:w-[186px] h-[490px]"/>
             </div>
           </template>
         </WallOfText>
@@ -12,7 +12,7 @@
           <svg class="absolute -mt-20 -ml-32 fill-current text-white">
             <circle r="50px" cx="50%" cy="50%"/>
           </svg>
-          <div class="text-red-500 font-bold text-4xl absolute -mt-10">
+          <div class="text-cf-red-500 font-brand font-bold text-4xl absolute -mt-10">
             me
           </div>
         </div>
